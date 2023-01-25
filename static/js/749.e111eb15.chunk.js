a63c17c06552061868d2a7aceec0f32fc2a886a3
Velbrave <112870568+Velbrave@users.noreply.github.com>
2023-01-25 14:05:43 +0000

    Deploying to gh-pages from @ Velbrave/goit-react-hw-08-phonebook@0eb0e3c90075264482c8ba53a6644ed8edfbffea 🚀

---
+++ static/js/749.e111eb15.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[749],{749:function(o,e,t){t.r(e);t(2791);var n=t(1155),c=t(184);e.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("img",{src:n,alt:"Phonebook",width:"100%"})})}},1155:function(o,e,t){o.exports=t.p+"static/media/phonebook.b6cf75fb6fd5d3f84377.jpeg"}}]);
+//# sourceMappingURL=749.e111eb15.chunk.js.map
